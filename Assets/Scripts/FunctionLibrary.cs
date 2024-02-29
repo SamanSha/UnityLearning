f494934b4d146fcfb55a20c40dd3ab8480960e71
Saman <254458393@qq.com>
2024-03-01 01:52:44 +0800

    Complete "Building a Graph".

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FunctionLibrary
+{
+    public static float Wave(float x, float t) {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+}
