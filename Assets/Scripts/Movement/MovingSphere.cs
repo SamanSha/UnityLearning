f116d0bc84a5d2b2108bc68b4f73ef101ae9fd14
Saman <254458393@qq.com>
2024-05-17 13:46:11 +0800

    Complete "Orbit Camera".

--- Assets/Scripts/Movement/MovingSphere.cs
+++ Assets/Scripts/Movement/MovingSphere.cs
@@ -28,6 +28,9 @@ public class MovingSphere : MonoBehaviour {
     [SerializeField]
     LayerMask probeMask = -1, stairsMask = -1;
 
+    [SerializeField]
+    Transform playerInputSpace = default;
+
     Vector3 velocity, desiredVelocity;
 
     Rigidbody body;
@@ -64,8 +67,20 @@ public class MovingSphere : MonoBehaviour {
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
-        desiredVelocity = 
+        if (playerInputSpace) {
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = 
+                (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        }
+        else {
+            desiredVelocity =
             new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        }
 
         desiredJump |= Input.GetButtonDown("Jump");
 
